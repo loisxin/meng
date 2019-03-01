d9611f02d56606908fe99856cbe2021d620bf44e
githubloisxin <zhouxylois@gmail.com>
2019-03-01 13:53:59 -0500

    Initial commit

---
+++ src/components/Tables/OrderedTable.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <md-table v-model="users" :table-header-color="tableHeaderColor">
+      <md-table-row slot="md-table-row" slot-scope="{ item }">
+        <md-table-cell md-label="ID">{{ item.id }}</md-table-cell>
+        <md-table-cell md-label="Name">{{ item.name }}</md-table-cell>
+        <md-table-cell md-label="Category">{{ item.salary }}</md-table-cell>
+        <md-table-cell md-label="Position">{{ item.country }}</md-table-cell>
+        <md-table-cell md-label="Time">{{ item.city }}</md-table-cell>
+      </md-table-row>
+    </md-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ordered-table",
+  props: {
+    tableHeaderColor: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      selected: [],
+      users: [
+        {
+          id: 1,
+          name: "Dakota Rice",
+          salary: "A",
+          country: "Niger",
+          city: "Oud-Turnhout"
+        },
+        {
+          id: 2,
+          name: "Minerva Hooper",
+          salary: "B",
+          country: "Curaçao",
+          city: "Sinaai-Waas"
+        },
+        {
+          id: 3,
+          name: "Sage Rodriguez",
+          salary: "C",
+          country: "Netherlands",
+          city: "Overland Park"
+        },
+        {
+          id: 4,
+          name: "Philip Chaney",
+          salary: "D",
+          country: "Korea, South",
+          city: "Gloucester"
+        }
+      ]
+    };
+  }
+};
+</script>
